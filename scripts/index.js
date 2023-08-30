a073df20d3d2faa099f405f3c043a6e30a9ff56c
ATribbey <alx_036@yahoo.com>
2023-08-29 21:24:23 -0400

    Add Edit Button Functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditBtn = document.querySelector("#profile-edit-btn");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileModalClose = document.querySelector("#profile-modal-close");
+
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_closed");
+});
+
+profileModalClose.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_closed");
+});
