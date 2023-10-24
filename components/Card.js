07bb308aed59a9c1c557d83d527c112201a2b569
ATribbey <alx_036@yahoo.com>
2023-10-24 10:13:01 -0400

    Refactor and add Card Class

---
+++ components/Card.js
@@ -0,0 +1,44 @@
+export default class Card {
+  constructor({ name, link }, cardSelector, handleImageClick) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+    this._handleImageClick = handleImageClick;
+  }
+  _setEventListeners() {
+    this._cardElement
+      .querySelector(".card__button-like")
+      .addEventListener("click", this._likeHandler);
+
+    this._cardElement
+      .querySelector(".card__button-delete")
+      .addEventListener("click", this._deleteHandler);
+
+    this._cardImageElement = this._cardElement
+      .querySelector(".card__image")
+      .addEventListener("click", () => {
+        this._handleImageClick(this);
+      });
+  }
+
+  _likeHandler() {
+    this._cardElement
+      .querySelector(".card__button-like")
+      .classList.toggle("card__button-like_active");
+  }
+
+  _deleteHandler() {
+    this._cardElement.remove();
+  }
+
+  getCardElement() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    this._setEventListeners();
+
+    return this._cardElement;
+  }
+}
