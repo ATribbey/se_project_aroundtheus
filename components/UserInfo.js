b22ea4350d01ff85b27bf9c6abaaacf76d331d8d
ATribbey <alx_036@yahoo.com>
2023-11-08 17:13:46 -0500

    write userinfo class

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,8 +1,17 @@
 export default new (class UserInfo {
   constructor({ profileName, profileJob }) {
-    this._profileName = profileName;
-    this._profileJob = profileJob;
+    this._profileName = document.querySelector(profileName);
+    this._profileJob = document.querySelector(profileJob);
   }
-  getUserInfo() {}
-  setUserInfo() {}
+  getUserInfo() {
+    const userInfo = {
+      name: this._profileName.textContent,
+      job: this._profileJob.textContent,
+    };
+    return userInfo;
+  }
+  setUserInfo(data) {
+    this._profileName.textContent = data.name;
+    this._profileJob.textContent = data.job;
+  }
 })();
