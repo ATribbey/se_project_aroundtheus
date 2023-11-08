6237e256bba6da0dff95f8593c29d4ea8e9bfc9c
ATribbey <alx_036@yahoo.com>
2023-11-08 16:37:35 -0500

    add markup for userinfo class

---
+++ components/UserInfo.js
@@ -0,0 +1,8 @@
+export default new (class UserInfo {
+  constructor({ profileName, profileJob }) {
+    this._profileName = profileName;
+    this._profileJob = profileJob;
+  }
+  getUserInfo() {}
+  setUserInfo() {}
+})();
