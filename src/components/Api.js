ab594ec3bcd4043a0012a88e0142a9ed06b6f1e7
ATribbey <alx_036@yahoo.com>
2023-11-22 13:52:53 -0500

    add add and remove like methods to api

--- src/components/Api.js
+++ src/components/Api.js
@@ -21,7 +21,7 @@ export default class Api {
       });
   }
 
-  addNewCard() {
+  addNewCard({ name, link }) {
     fetch("https://around-api.en.tripleten-services.com/v1/cards", {
       method: "POST",
       headers: {
@@ -29,8 +29,8 @@ export default class Api {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        name: "Yosemite Valley",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+        name: name,
+        link: link,
       }),
     })
       .then((res) => {
@@ -53,6 +53,48 @@ export default class Api {
     })
       .then((res) => {
         if (res.okay) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  addCardLike(cardId) {
+    fetch(
+      `https://around-api.en.tripleten-services.com/v1/cards/${cardId}/likes`,
+      {
+        method: "PUT",
+        headers: {
+          authorization: "97f6b443-08dd-4f1c-9189-025b318b185d",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  removeCardLike(cardId) {
+    fetch(
+      `https://around-api.en.tripleten-services.com/v1/cards/${cardId}/likes`,
+      {
+        method: "DELETE",
+        headers: {
+          authorization: "97f6b443-08dd-4f1c-9189-025b318b185d",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
           return res.json();
         }
         return Promise.reject(`Error: ${res.status}`);
